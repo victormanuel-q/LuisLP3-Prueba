74ab96ea0e06cdc1079eb987cfd582840d0853cd
Luis Angel Izquierdo <luisangel19992412@gmail.com>
2020-07-09 15:04:45 -0500

    Se creo la funcion determinaraprobado

---
+++ aprobado.py
@@ -0,0 +1,10 @@
+#Ejercicio que determine si un estudiante esta aprobado o no
+#AUTOR: LUIS ANGEL IZQUIERDO ROJAS
+
+def determinaraprobado(promedio):
+    if promedio>=11:
+        resultado = "Aprobado"
+    else:
+        resultado = "Desaprobado"
+    return resultado
+
